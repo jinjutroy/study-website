a0199bf3a7118f02ba238d049387a26e34555950
Uyen Nguyen <nguyenuyen552000@gmail.com>
2022-06-02 15:06:41 +0700

    commit router home

--- src/app/views/homepage/home-routing.module.ts
+++ src/app/views/homepage/home-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home.component'; 
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent, 
-  }
+    component: HomeComponent,
+  },{ path: 'home',   redirectTo: '' ,pathMatch: 'full'}
 ];
 
 @NgModule({
