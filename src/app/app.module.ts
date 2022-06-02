5d7137197b2ba3c5250fb9a1591be2840b48d70d
Bùi Ngọc Yên <buingocyen055@gmail.com>
2022-06-03 00:38:24 +0700

    feature/UI-003 Updated route and UI-002 Creating UI page lession

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
- 
-import { NgModule } from '@angular/core';
+import { LessionModule } from './views/lession/lession.module';
+import { HomeModule } from './views/homepage/home.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './views/shared/shared.module';
+ 
+import { NgModule } from '@angular/core'; 
 import {
   FooterComponent,
   HeaderComponent,
 } from './views/shared/layouts'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeRoutingModule } from './views/homepage/home-routing.module'; 
+import { HomeRoutingModule } from './views/homepage/home-routing.module';
+import { LessionComponent } from './views/lession/lession.component'; 
 
 @NgModule({ 
   declarations: [AppComponent, FooterComponent, HeaderComponent],
-  imports: [ 
+  imports: [   
+    HomeModule,
     BrowserModule,
     AppRoutingModule,
     HomeRoutingModule,
-  ],
-  providers: [],
+    SharedModule, 
+    LessionModule,
+  ],  
+  providers: [], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
