b673fa7541a18d0a18e7a16e886e48b82e4088fc
Bùi Ngọc Yên <buingocyen055@gmail.com>
2022-06-04 17:17:48 +0700

    feature/UI-002-UPDATE-UI-PAGE-LESSION

--- src/app/views/shared/layouts/header.component.ts
+++ src/app/views/shared/layouts/header.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  
-}) 
-export class HeaderComponent  { 
-    img: string = 'assets/images/public/logo.png';
+
+})
+export class HeaderComponent implements OnInit {
+  img: string = 'assets/images/public/logo.png';
+
+  public href: string = "";
+  public active = "";
+  constructor(private Location: Location) {
+  }
+  ngOnInit() {
+    this.href = this.Location.path();
+    this.active= this.href.slice(1);
+    console.log(this.href);
+    
+  } 
+  handlerClickNavbar = (e: any) => {
+    const listItem = document.querySelectorAll(".nav-link");
+    listItem.forEach(e => e.classList.remove("active"));
+    this.active = e.target.id; 
+  }
 };
