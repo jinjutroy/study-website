e8f8b36ae8263eb6c4dfdd39a2c0b087e4eac400
Bùi Ngọc Yên <buingocyen055@gmail.com>
2022-06-10 16:59:09 +0700

    resolve conflict

--- src/app/services/lession/lession.service.ts
+++ src/app/services/lession/lession.service.ts
@@ -33,6 +33,7 @@ export class LessionService {
   getLessionByName(name: string): Observable<BaiGiang[]>{
     return this.httpClient.get<BaiGiang[]>(apiLessionByIdUrl+"search?name="+name);
   }
+
   createLession(baiGiang:BaiGiangRequest){
     return this.httpClient.post<BaiGiangRequest>(apiLessionByIdUrl+'create',baiGiang)
   }
