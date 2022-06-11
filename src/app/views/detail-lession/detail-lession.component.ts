2cdfce0eb4c2dfc8de00f526c2262eebae22af8f
Uyen Nguyen <nguyenuyen552000@gmail.com>
2022-06-11 15:10:27 +0700

    fix lession video

--- src/app/views/detail-lession/detail-lession.component.ts
+++ src/app/views/detail-lession/detail-lession.component.ts
@@ -3,6 +3,7 @@ import { LessionService } from '../../services/lession/lession.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-detail-lession',
@@ -13,12 +14,23 @@ export class DetailLessionComponent implements OnInit {
 
   private routeSub: Subscription | undefined;
   lession: BaiGiang[] = [];
-  constructor(private route: ActivatedRoute, private _lessionService: LessionService) { }
+  constructor(private route: ActivatedRoute,
+              private _lessionService: LessionService,
+              private sanitizer: DomSanitizer) { }
   id=0;
+  link: string = '';
   ngOnInit() {
-    this.routeSub = this.route.params.subscribe(params => { 
-      this.id = params['id']; 
+    this.routeSub = this.route.params.subscribe(params => {
+      this.id = params['id'];
     });
-    this._lessionService.getLessionById(this.id).subscribe(response => this.lession.push(response));  
+    this._lessionService.getLessionById(this.id).subscribe(response => {this.lession.push(response);
+    this.link=response.fileVideo;
+    });
+  }
+  transform() {
+// @ts-ignore
+    let url = this.link;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+
   }
 }
