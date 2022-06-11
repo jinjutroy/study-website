55918c0bbbce67056c8e4e565e4c78d1a3948132
Bùi Ngọc Yên <buingocyen055@gmail.com>
2022-06-11 10:09:36 +0700

    update firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+   production: true,
    firebaseConfig :{
     apiKey: "AIzaSyAxPWMHS9oCK4mbZoFryn-vLvKdunABfhE",
     authDomain: "notifications-with-fireb-84940.firebaseapp.com",
