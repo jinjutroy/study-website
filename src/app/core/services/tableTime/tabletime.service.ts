5fc1969190f5dfbb842df496a99aa52ca0bbcf47
Uyen Nguyen <nguyenuyen552000@gmail.com>
2022-06-14 08:07:00 +0700

    manager quản lí thời khoá biểu

--- src/app/core/services/tableTime/tabletime.service.ts
+++ src/app/core/services/tableTime/tabletime.service.ts
@@ -4,6 +4,7 @@ import {Observable} from "rxjs";
 import {ChiTietThoiKhoaBieu} from "../../model/ChiTietThoiKhoaBieu";
 import {AuthService} from "../pagelogin/auth.service";
 import {ThoiKhoaBieuDTO} from "../../dto/ThoiKhoaBieuDTO";
+import {ThoiKhoaBieuCreateDTO} from "../../dto/ThoiKhoaBieuCreateDTO";
 
 @Injectable({
   providedIn: 'root'
@@ -27,5 +28,10 @@ export class TabletimeService {
   // update thoi khoa bieu
   updateTimeTable(thoikhoabieu: ThoiKhoaBieuDTO[]) {
     return this.http.post(this.api + '/tb/update', thoikhoabieu, {headers: this.headers})
+  }
+
+// create thoi khoa bieu
+  createTimeTable(thoikhoabieu: ThoiKhoaBieuCreateDTO[],idlop:number) {
+    return this.http.put(this.api + '/tb/create/'+idlop, thoikhoabieu, {headers: this.headers})
   }
 }
